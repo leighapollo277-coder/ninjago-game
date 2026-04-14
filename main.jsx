56e7a9a88fc059b74258d89d006e6cbf3fd4d698
Kenneth <kenneth@Kenneth-MacBook.local>
2026-04-14 19:46:31 +0800

    fix: restore entry point to NinjagoGameApp to enable dashboard

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './MapCinematicEngine.jsx'
+import App from './NinjagoGameApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
